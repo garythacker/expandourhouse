ac6df2c22deea14e8fc369993dda2e2b7abf78a6
garythacker <garythacker227@gmail.com>
2020-02-16 19:15:01 -0800

    src/include.mk: Bugfix in how generated sources are made

--- map-data/src/include.mk
+++ map-data/src/include.mk
@@ -4,6 +4,8 @@ GO_LIB_SOURCES := \
 	$(wildcard src/states/*.go) \
 	$(wildcard src/stylemetadata/*.go) \
 	$(wildcard src/utils/*.go) \
+	src/states/states.go \
+	src/cmd/make-style/styleTemplate.go \
 	src/go.mod \
 	src/go.sum
 
@@ -18,16 +20,28 @@ PROGRAMS = \
 
 GO = GOPATH="${TMP}" go
 
+src/states/states.go: src/states/scripts/makeStates.go src/states/placeholder.go
+	cd src/states && ${GO} generate
+
+src/cmd/make-style/styleTemplate.go: src/cmd/make-style/scripts/includeStyle.go \
+	src/cmd/make-style/main.go src/cmd/make-style/style-template.json
+	cd src/cmd/make-style && ${GO} generate
+
 define PROGRAM_TARGETS
 $${TMP}/${prog}: $$(wildcard src/cmd/${prog}/*.go) $${GO_LIB_SOURCES}
-	cd "src/cmd/${prog}" && $${GO} generate && $${GO} build -o "$$@"
+	cd "src/cmd/${prog}" && $${GO} build -o "$$@"
 
 clean-${prog}:
 	cd "src/cmd/${prog}" && $${GO} clean
+
+.PHONY: clean-${prog}
 endef
 
 $(foreach prog,${PROGRAMS},$(eval ${PROGRAM_TARGETS}))
 
-.PHONY: clean-programs
-clean-programs:
+.PHONY: programs clean-programs
+
+programs: $(patsubst %,${TMP}/%,${PROGRAMS})
+
+clean-programs: $(patsubst %,clean-%,${PROGRAMS})
 	rm -f src/cmd/make-style/styleTemplate.go src/states/states.go
