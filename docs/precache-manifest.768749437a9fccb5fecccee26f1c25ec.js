1e63111715c9d5af404a25761a03e4e9539b5d6e
garythacker <garythacker227@gmail.com>
2020-06-04 18:47:01 -0700

    Add docs dir for GitHub Pages

---
+++ docs/precache-manifest.768749437a9fccb5fecccee26f1c25ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a14ee7c00c40d1fa85d692f43ede6aa2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f561f02602f569c0e5b9",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "5b4eb8d33acefa8424a5",
+    "url": "/static/js/2.ca5cefc4.chunk.js"
+  },
+  {
+    "revision": "56437791f6597d025588c3fd11f5c056",
+    "url": "/static/js/2.ca5cefc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f561f02602f569c0e5b9",
+    "url": "/static/js/main.71618b10.chunk.js"
+  },
+  {
+    "revision": "4305ad129dcf89844c80",
+    "url": "/static/js/runtime-main.3ea18b6e.js"
+  }
+]);
